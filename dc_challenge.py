34c78aae156d459ded58e93dfd75e69d1be291fe
Anna Sedlar <annasedlar@gmail.com>
2017-02-20 11:04:16 -0500

    DigitalCrafts javascript code challenge, in Python

---
+++ dc_challenge.py
@@ -0,0 +1,87 @@
+# 1) Declare two variables, a string and an integer 
+# named "fullName" and "age". Set them equal to your name and age.
+
+full_name = "Anna Sedlar"; 
+age = 24; 
+
+
+# 2) Declare an empty array called "myArray". 
+# Add the variables from #1 (fullName and age) to the empty array using the push method.
+# Print to the console.
+
+my_array = [];
+my_array.insert(0, full_name);
+my_array.insert(1, age);
+print my_array;
+
+
+# 3) Write a simple function that takes no parameters called "sayHello". 
+# Make it print "Hello!" to the console when called.
+# Call the function.
+
+def sayHello():
+	print "Hello!";
+sayHello(); 
+
+# 4) Declare a variable named splitName and set it equal to
+# fullName split into two seperate objects in an array.
+# (In other words, if the variable fullName is equal to "John Smith", then splitName should 
+# equal ["John", "Smith"].)
+# Print splitName to the console.
+# HINT: Remember to research the methods and concepts listed in the instructions PDF.
+
+split_name = full_name.split(); 
+print split_name; 
+
+
+# 5) Write another simple function that takes no parameters called "sayName".
+# When called, this function should print "Hello, ____!" to the console, where the blank is 
+# equal to the first value in the splitName array from #4.
+# Call the function.  (In our example, "Hello, John!" would be printed to the console.)
+
+def say_name(name): 
+	print "Hello " + name + "!";
+
+say_name("Anna"); 
+
+
+# 6) Write another function named myAge.  This function should take one parameter: the year you 
+# were born, and it should print the implied age to the console.
+# Call the function, passing the year you were born as the argument/parameter.
+# HINT: http://www.w3schools.com/js/js_functions.asp
+ 
+import datetime
+from datetime import date
+print date.today()
+def my_age(birthyear): 
+	today = date.today()
+	print today.year - birthyear
+
+my_age(1992); 
+
+# 7) Using the basic function given below, add code so that sum_odd_numbers will print to the console
+# the sum of all the odd numbers from 1 to 5000.  Don't forget to call the function!
+# HINT: Consider using a 'for loop'.
+ 
+# function sum_odd_numbers() {
+#     var sum = 0;
+
+#     // Write your code here
+#     console.log(sum);
+# }
+
+def sum_odd_numbers(): 
+	count = 0 
+	for i in range(1, 5000): 
+		if i % 2 == 1:
+			count += i;  
+	print(count); 
+	
+sum_odd_numbers();
+
+
+
+
+
+
+
---
+++ the_basics.py
@@ -0,0 +1,96 @@
+print "Anna Sedlar"
+
+name = "Anna Sedlar" 
+
+# lists = arrays
+animals = ['wolf', 'giraffe', 'hippo']
+print animals
+
+# this will go one from the end of the array ie. hippo
+print animals[-1] 
+
+# array.push == list.append
+animals.append("Alligator");
+print animals
+
+# to delete an index, use remove method
+animals.remove('wolf'); 
+print animals
+
+# alternatively, use del
+del animals[0];
+
+# insert into any idicie with 'insert'
+animals.insert(0, 'zebra');
+print animals
+
+animals[0] = 'horse'; 
+print animals
+
+# pop works like js pop - removes last element of a list
+animals.pop()
+print animals
+animals.pop()
+print animals
+
+
+
+
+
+dc_class = "Michael, Paul, Mason, Casey, Connie, Sarah, Andy"; 
+dc_class_as_array = dc_class.split(", "); 
+print dc_class_as_array
+
+# sorted method will sort, but NOT CHANGE the actual array
+print sorted(dc_class_as_array); 
+
+# so you must assign it a variable
+sorted_class = sorted(dc_class_as_array)
+
+dc_class_as_array.sort()
+print dc_class_as_array
+print sorted
+print dc_class_as_array
+
+# reverse method reverse the array not by value but by index
+dc_class_as_array.reverse();
+print dc_class_as_array;
+
+# length
+print len(dc_class_as_array)
+
+# slice [x:x]
+# creates a copy of the array, does not mutate the array
+print dc_class_as_array[2:4]
+
+for student in dc_class_as_array:
+	print student
+
+for i in range(1, 10):
+	print i; 
+
+for i in range(1, len(dc_class_as_array)):
+	print dc_class_as_array
+
+# A function in python is NOT a function. It is a definition (def) 
+def say_hello():
+	# just like for loops, colon is the curly brace in python for functions
+	# indentation matters, just like for loop
+	print "Hello World"; 
+
+say_hello(); 
+
+def say_hello_with_name(name): 
+	print "hello, " + name; 
+
+say_hello_with_name("Anna"); 
+
+
+
+
+
+
+
+
+
+
